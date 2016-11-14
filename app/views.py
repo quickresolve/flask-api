997120d7956e8ce2d75bda56179f4bfaff735fdd
QuickResolve <lauranmontoya@gmail.com>
2016-11-13 17:07:48 -0800

    create basic flask app to serve templates or api

---
+++ app/views.py
@@ -0,0 +1,31 @@
+from flask import *
+from app import app
+
+user = {'name': 'guest'} #fake user
+posts = [  # fake array of posts
+    {
+        'author': {'nickname': 'John'},
+        'body': 'Beautiful day in Portland!'
+    },
+    {
+        'author': {'nickname': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    }
+]
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+  return render_template('index.html',
+                         title='Home',
+                         user=user,
+                         posts=posts)
+
+
+@app.route('/api')
+def api():
+  return jsonify({'posts': posts})
+
+
+# render_template function invokes the Jinja2 templating engine that is part of the Flask framework. Jinja2 substitutes {{...}} blocks with the corresponding values provided as template arguments. The Jinja2 templates also support control statements, given inside {%...%} blocks.
---
+++ run.py
@@ -0,0 +1,4 @@
+from app import app
+app.run(debug=True)
+
+'''The script simply imports the app variable from our app package and invokes its run method to start the server.'''
